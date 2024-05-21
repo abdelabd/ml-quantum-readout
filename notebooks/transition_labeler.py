024184278f3cb0cdc4c4f86d218879a6ef411e6d
Abdel FNAL <aelabd@correlator3.fnal.gov>
2024-05-21 15:37:23 -0500

    bug fix: compare to r2_0_1 insteadf

--- notebooks/transition_labeler.py
+++ notebooks/transition_labeler.py
@@ -10,7 +10,7 @@ class TransitionLabeler(object):
         self.I1_mean = None
         self.Q1_mean = None
 
-    def fit_and_label(self, X, y):
+    def fit(self, X, y):
         idx0 = np.where(y==0)[0]
         idx1 = np.where(y==1)[0]
 
@@ -18,28 +18,29 @@ class TransitionLabeler(object):
         X1 = X[idx1]
 
         X0_I_tmean, X0_Q_tmean = self.split_and_time_average(X0)
-        X0_centroid = (np.mean(X0_I_tmean), np.mean(X0_Q_tmean))
+        self.X0_centroid = (np.mean(X0_I_tmean), np.mean(X0_Q_tmean))
 
         X1_I_tmean, X1_Q_tmean = self.split_and_time_average(X1)
-        X1_centroid = (np.mean(X1_I_tmean), np.mean(X1_Q_tmean))
+        self.X1_centroid = (np.mean(X1_I_tmean), np.mean(X1_Q_tmean))
 
-        r2_0_1 = (X0_centroid[0] - X1_centroid[0])**2 + (X0_centroid[1] - X1_centroid[1])**2
+        self.r2_0_1 = (self.X0_centroid[0] - self.X1_centroid[0])**2 + (self.X0_centroid[1] - self.X1_centroid[1])**2
+        
+    def label(self, X, y):
+        idx0 = np.where(y==0)[0]
+        idx1 = np.where(y==1)[0]
 
         idx_excite = []
         for idx in idx0:
             X_i = X[idx]
-
-            r2_0_i = (X0_centroid[0] - X_i[0])**2 + (X0_centroid[1] - X_i[1])**2
-            r2_1_i = (X1_centroid[0] - X_i[0])**2 + (X1_centroid[1] - X_i[1])**2
-            if r2_0_i < r2_1_i:
+            r2_1_i = (self.X1_centroid[0] - X_i[0])**2 + (self.X1_centroid[1] - X_i[1])**2
+            if r2_1_i <= self.r2_0_1: # r2_1_i:
                 idx_excite.append(idx)
 
         idx_relax = []
         for idx in idx1:
             X_i = X[idx]
-            r2_0_i = (X0_centroid[0] - X_i[0])**2 + (X0_centroid[1] - X_i[1])**2
-            r2_1_i = (X1_centroid[0] - X_i[0])**2 + (X1_centroid[1] - X_i[1])**2
-            if r2_0_i > r2_1_i:
+            r2_0_i = (self.X0_centroid[0] - X_i[0])**2 + (self.X0_centroid[1] - X_i[1])**2
+            if r2_0_i <= self.r2_0_1:
                 idx_relax.append(idx)
 
         y[idx_excite] = 2
